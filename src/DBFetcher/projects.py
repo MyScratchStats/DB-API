3f0c1ed6e488c20c4c902b8dc7ae5e5122141e8a
kRxZykRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-06-16 18:07:04 +0100

    Update projects.py

--- src/DBFetcher/projects.py
+++ src/DBFetcher/projects.py
@@ -3,11 +3,10 @@ import requests
 stats = []
 i = 0
 while True:
-  res = requests.get(f'https://api.scratch.mit.edu/projects/{i}')
-  if res.status_code != 200:
-    print("404 Not Found")
-  else:
-    stats[i] = res.json()
-    print(stats)
-  i += 1
-
+    res = requests.get(f'https://api.scratch.mit.edu/projects/{i}')
+    if res.status_code != 200:
+        print(f"{i}: 404 Not Found")
+    else:
+        stats.append(res.json())
+        print(stats[-1])  # Safely print the last item added
+    i += 1
